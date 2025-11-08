ea9ad9d5f3aa5821afd59339613da671bb83a137
vmthai111 <tvmtpon@gmail.com>
2025-11-08 22:48:56 +0700

    Remove defaultHeartImages from script.js
    Removed default heart images initialization.

--- script.js
+++ script.js
@@ -84,8 +84,6 @@ const galaxyParameters = {
   outsideColor: new THREE.Color(0x48b8b8),
 };
 
-const defaultHeartImages = Array.from({ length: 2 }, (_, i) => `images/img${i + 1}.jpg`);
-
 const heartImages = [
   ...(window.dataCCD?.data?.heartImages || []),
   ...defaultHeartImages,
