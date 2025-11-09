6e7fb89afe31f4edb39c61c91fc2dd550f7f783d
vmthai111 <tvmtpon@gmail.com>
2025-11-09 12:53:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ const galaxyParameters = {
   outsideColor: new THREE.Color(0x48b8b8),
 };
 
-const defaultHeartImages = Array.from({ length: 2 }, (_, i) => `images/img${i + 1}.jpg`);
+const defaultHeartImages = [];
 
 const heartImages = [
   ...(window.dataCCD?.data?.heartImages || []),
